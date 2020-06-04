d2c65e4a3bfbb0daaae830f8a2dd3aa1352fed8f
Leonardo Rossi <lrossi@solstice.com>
2020-06-03 22:57:21 -0300

    Adding webpack.

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const webpack = require('webpack');
+const path = require('path');
+const TerserJSPlugin = require('terser-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: [
+      './src/scripts/index.js',
+      './src/styles/main.css'
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'bundle.[chunkhash].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              outputPath: 'assets/images',
+              name: '[name].[ext]',
+            }
+          },
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      }
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'styles.[hash].css'
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      favicon: 'src/assets/images/favicon.ico',
+      scriptLoading: 'defer'
+    })
+  ],
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
+  },
+}
